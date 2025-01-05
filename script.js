2101e188ad3b3baf39cdc826ccbc65c3ff779ecd
Alves Hatewa <alvesmazy90@gmail.com>
2025-01-05 19:54:25 +0100

    Adicionadas outras secções na estrutura do site

--- script.js
+++ script.js
@@ -63,3 +63,40 @@ document.addEventListener('DOMContentLoaded', function() {
         menu.classList.toggle('active');
     });
 });
+
+// Animação de fade up para os itens de serviço
+document.addEventListener('DOMContentLoaded', () => {
+    const services = document.querySelectorAll('.item-service');
+    services.forEach((service, index) => {
+        setTimeout(() => {
+            service.style.opacity = 1;
+            service.style.animationDelay = `${index * 0.5}s`;
+        }, index * 500);
+
+        const toggleArrow = service.querySelector('.toggle-arrow');
+        const description = service.querySelector('.description');
+
+        toggleArrow.addEventListener('click', () => {
+            if (description.style.display === 'none' || description.style.display === '') {
+                description.style.display = 'block';
+                toggleArrow.textContent = '▲'; // Mudar para seta para cima
+            } else {
+                description.style.display = 'none';
+                toggleArrow.textContent = '▼'; // Mudar para seta para baixo
+            }
+        });
+    });
+});
+
+// Mostrar ou ocultar a descrição dos itens de serviço
+
+function toggleDescription(button) {
+    const description = button.previousElementSibling;
+    if (description.style.display === "none" || description.style.display === "") {
+        description.style.display = "block";
+        button.textContent = "▲";
+    } else {
+        description.style.display = "none";
+        button.textContent = "▼";
+    }
+}
